f53b041a9aa85dbdbeac5551ef03bc12227a5145
Rio Kwok <riotkkwok@gmail.com>
2015-08-26 21:21:28 +0800

    Create CokeUtil.js
    Initial

---
+++ CokeUtil.js
@@ -0,0 +1,146 @@
+/*
+ * CokeUtil - a set of utility methods
+ *
+ * Released under the MIT license
+ *
+ * Auther: Rio Kwok
+ *
+ * Version: 0.0.1
+ *
+ * Updates: 
+ * 1. Initial
+ *
+ */
+
+;(function(){
+    var CokeUtil = function(){};
+
+    var ua = navigator.userAgent;
+
+    /**
+     * Specify the format(string) to parse the date object to string.
+     *
+     * @param {Date} Date object to be parsed
+     * @param {string} The format to be returned,
+     *        Y - one digit of year
+     *        M - one digit of month
+     *        D - one digit of day
+     *        h - one digit of hour
+     *        m - one digit of minute
+     *        s - one digit of second
+     * @returns {string}
+     */
+    CokeUtil.prototype.formatDate = function(date, format){
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        var result = format, tmpObj = {};
+
+        if(!date instanceof Date){
+            try{
+                console.error('The argument \'date\' is not a Date Object.');
+            }catch(e){}
+            return '';
+        }
+        if(typeof format !== 'string'){
+            try{
+                console.error('The argument \'format\' is not a string.');
+            }catch(e){}
+            return '';
+        }
+
+        function toString(ch, val){
+            var start = result.indexOf(ch);
+            if(start < 0){
+                return '';
+            }
+            for(var i=1; i<result.length; i++){
+                if(result[start + i] !== ch){
+                    break;
+                }
+            }
+            if(ch === 'M'){
+                if(i > 3){
+                    result = result.replace(result.substring(start, start + i), months[val-1]);
+                }else if(i === 3){
+                    result = result.replace(result.substring(start, start + i), months[val-1].substr(0, i));
+                }else{
+                    result = result.replace(result.substring(start, start + i), ('0'+val).toString().substr(-1 * i));
+                }
+            }else if(ch === 'Y'){
+                result = result.replace(result.substring(start, start + i), val.toString().substr(-1 * i));
+            }else{
+                result = result.replace(result.substring(start, start + i), ('0'+val).toString().substr(-1 * i));
+            }
+        }
+
+        tmpObj.year = date.getFullYear();
+        tmpObj.month = date.getMonth() + 1;
+        tmpObj.day = date.getDate();
+        tmpObj.hour = date.getHours();
+        tmpObj.minute = date.getMinutes();
+        tmpObj.second = date.getSeconds();
+
+        toString('Y', tmpObj.year)
+        toString('D', tmpObj.day)
+        toString('h', tmpObj.hour)
+        toString('m', tmpObj.minute)
+        toString('s', tmpObj.second)
+        toString('M', tmpObj.month);
+
+        return result;
+    };
+
+
+    /**
+     * Compare the versions.
+     *
+     * @param {string} version number string, likes 10.1, 2.3.4, 1.0.0.2
+     * @param {string} version number string, likes 10.1, 2.3.4, 1.0.0.2
+     * @returns {number|null} -1, 0, 1
+     */
+    CokeUtil.prototype.compareVers = function(ver1, ver2){
+        if(typeof ver1 !== 'string' || typeof ver2 !== 'string' || ver1.length === 0 || ver2.length === 0){
+            return null;
+        }
+        if(ver1 === ver2){
+            return 0;
+        }
+        var re1 = ver1.split('.'), re2 = ver2.split('.');
+        for(var i = 0; i<re1.length && i<re2.length; i++){
+            if(parseInt(re1[i], 10) > parseInt(re2[i], 10)){
+                return 1;
+            }else if(parseInt(re1[i], 10) < parseInt(re2[i], 10)){
+                return -1;
+            }
+        }
+        if(parseInt(re1[i] || '0', 10) > parseInt(re2[i] || '0', 10)){
+            return 1;
+        }else if(parseInt(re1[i] || '0', 10) < parseInt(re2[i] || '0', 10)){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+
+
+    /**
+     * user agent info
+     *
+     */
+    CokeUtil.prototype.userAgent = {
+        isIOS: /iPhone|iPod|iPad/i.test(ua),
+        isAndroid: /Android/i.test(ua),
+        isWeiXin: /MicroMessenger/i.test(ua),
+        isChrome: /chrome/i.test(ua),
+        isUCWEB: /UCWEB|UCBrowser/i.test(ua),
+        isOpera: /Opera|Oupeng/i.test(ua),
+        isFireFox: /Firefox/i.test(ua),
+        isQQBrowser: /MQQBrowser/i.test(ua),
+        is360Browser: /360 Aphone Browser/i.test(ua),
+        isLieBao: /LieBao/i.test(ua),
+        isSmartis: /SANFRANCISCO/i.test(ua),
+        isBaiduBrowser: /baidubrowser/i.test(ua),
+    }
+
+
+    window.CokeUtil = CokeUtil;
+})();
